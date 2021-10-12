28533193ae3f45fa1db45ee02f78361b93535965
fabr1z10 <fabventurini@gmail.com>
2021-10-12 09:23:05 +0200

    more opcodes

--- c64.cpp
+++ c64.cpp
@@ -2,21 +2,217 @@
 
 C64::C64() {
 
+    m_opcodes[0x08] = [&] (uint16_t i) { this->php(i); };
+    m_opcodes[0x18] = [&] (uint16_t i) { this->clc(i); };
+    m_opcodes[0x28] = [&] (uint16_t i) { this->plp(i); };
+    m_opcodes[0x38] = [&] (uint16_t i) { this->sec(i); };
+    m_opcodes[0x48] = [&] (uint16_t i) { this->pha(i); };
+    m_opcodes[0x58] = [&] (uint16_t i) { this->cli(i); };
+    m_opcodes[0x68] = [&] (uint16_t i) { this->pla(i); };
+    m_opcodes[0x78] = [&] (uint16_t i) { this->sei(i); };
+    m_opcodes[0x88] = [&] (uint16_t i) { this->dey(i); };
+    m_opcodes[0x98] = [&] (uint16_t i) { this->tya(i); };
+    m_opcodes[0xA8] = [&] (uint16_t i) { this->tay(i); };
+    m_opcodes[0xB8] = [&] (uint16_t i) { this->clv(i); };
+    m_opcodes[0xC8] = [&] (uint16_t i) { this->iny(i); };
+    m_opcodes[0xD8] = [&] (uint16_t i) { this->cld(i); };
+    m_opcodes[0xE8] = [&] (uint16_t i) { this->inx(i); };
+    m_opcodes[0xF8] = [&] (uint16_t i) { this->sed(i); };
+
 
 }
 
+void C64::setNegativeFlag(uint8_t b) {
+    if (b & 0x80) {       // negative
+        m_sr |= 0x80;
+    } else {
+        m_sr &= 0x7F;
+    }
+}
+
+void C64::setZeroFlag(uint8_t b) {
+    if (b == 0) {
+        m_sr |= 0x02;
+    } else {
+        m_sr &= 0xFD;
+    }
+}
+
 // indexed-indirect addressing X
-uint8_t C64::inx(uint8_t offset) {
+uint8_t C64::addr_inx(uint8_t offset) {
     uint8_t zpa = m_x + offset;
     return m_memory[vecAsInt(zpa)];
 }
 
 // indexed-indirect addressing Y
-uint8_t C64::iny(uint8_t offset) {
+uint8_t C64::addr_iny(uint8_t offset) {
     uint8_t zpa = m_y + offset;
     return m_memory[vecAsInt(zpa)];
 }
 
 uint16_t C64::vecAsInt(uint16_t address) {
     return m_memory[address] + m_memory[address+1] * 256;
+}
+
+Vector C64::getVec(uint16_t i) {
+    return Vector{m_memory[i], m_memory[i+1]};
+}
+
+uint16_t Vector::toInt() const {
+    return lo + hi * 256;
+}
+
+/* CLC (short for "CLear Carry") is the mnemonic for a machine language instruction which unconditionally clears the
+ * carry flag.
+*/
+void C64::clc(uint16_t) {
+    m_sr &= 0xFE;
+}
+
+/* SEC (short for "SEt Carry") is the mnemonic for a machine language instruction which unconditionally sets the carry
+ * flag.
+ */
+void C64::sec(uint16_t) {
+    m_sr |= 0x01;
+}
+
+/* PHP (short for "PusH Processor flags") is the mnemonic for a machine language instruction which stores the current
+ * state of the processor status flags onto the stack, and adjusting the stack pointer to reflect the addition.
+ */
+void C64::php(uint16_t) {
+    m_memory[m_sp] = m_sr;
+    m_sp--;
+    // TODO check stack overflow
+
+}
+
+/* PLP (short for "PulL Processor flags") is the mnemonic for a machine language instruction which retrieves a set
+ * of status flags previously "pushed" onto the stack (usually by a PHP instruction) from the stack, and adjusting
+ * the stack pointer to reflect the removal of a byte.
+ */
+void C64::plp(uint16_t) {
+    m_sp++;
+    m_sr = m_memory[m_sp];
+    // TODO check stack underflow
+}
+
+/* PHA (short for "PusH Accumulator") is the mnemonic for a machine language instruction which stores a copy of the
+ * current content of the accumulator onto the stack, and adjusting the stack pointer to reflect the addition.
+ */
+void C64::pha(uint16_t) {
+    m_memory[m_sp] = m_a;
+    m_sp--;
+    // TODO check stack overflow
+}
+
+/* PLA (short for "PulL Accumulator") is the mnemonic for a machine language instruction which retrieves a byte from
+ * the stack and stores it in the accumulator, and adjusts the stack pointer to reflect the removal of that byte.
+ */
+void C64::pla(uint16_t) {
+    m_sp++;
+    m_a = m_memory[m_sp];
+
+    // The negative status flag is set if the retrieved byte is negative, i.e. has its most significant bit set.
+    setNegativeFlag(m_a);
+
+    // The zero flag is set if the retrieved byte is zero, or cleared if it is non-zero.
+    setZeroFlag(m_a);
+}
+
+/* CLI (short for "CLear Interrupt flag") is the mnemonic for a machine language instruction which clears the interrupt
+ * flag, so that the CPU will respond to IRQ interrupt events. To disable the response to IRQs, use the complementary
+ * instruction SEI.
+ */
+void C64::cli(uint16_t) {
+    m_sr &= 0xFB;
+}
+
+/* SEI (short for "SEt Interrupt flag") is the mnemonic for a machine language instruction which sets the interrupt
+ * flag, thereby preventing the CPU from responding to IRQ interrupt events. To re-enable the response to IRQs, use the
+ * complementary instruction CLI.
+ */
+void C64::sei(uint16_t) {
+    m_sr |= 0x04;
+}
+
+/* DEY (short for "DEcrease Y") is the mnemonic for a machine language instruction which decrements the numerical value
+ * of Y index register, by one, and "wraps over" if the value goes below the numerical limits of a byte.
+ */
+void C64::dey(uint16_t) {
+    m_y--;
+    setNegativeFlag(m_y);
+    setZeroFlag(m_y);
+}
+
+/* TYA (short for "Transfer Y to Accumulator") is the mnemonic for a machine language instruction which transfers
+ * ("copies") the contents of the Y index register into the accumulator.
+ */
+void C64::tya(uint16_t) {
+    m_a = m_y;
+
+    // The negative status flag is set if the byte transferred is negative, i.e. has it's most significant bit set.
+    setNegativeFlag(m_a);
+
+    // The zero flag is set if the result is zero, or cleared if it is non-zero.
+    setZeroFlag(m_a);
+}
+
+/* TAY (short for "Transfer Accumulator to Y") is the mnemonic for a machine language instruction which transfers
+ * ("copies") the contents of the accumulator into the Y index register.
+ */
+void C64::tay(uint16_t) {
+   m_y = m_a;
+
+   // The negative status flag is set if the byte transferred is negative, i.e. has it's most significant bit set.
+   setNegativeFlag(m_y);
+
+   // The zero flag is set if the byte transferred is zero, or cleared if it is non-zero.
+   setZeroFlag(m_y);
+
+}
+
+/* CLV (short for "CLear oVerflow") is the mnemonic for a machine language instruction which clears the Overflow flag.
+ */
+void C64::clv(uint16_t) {
+    m_sr &= 0xBF;
+}
+
+/* INY (short for "INcrease Y") is the mnemonic for a machine language instruction which increases the numerical value
+ * held in the Y index register by one, and "wraps over" when the numerical limits of a byte are exceeded.
+ */
+void C64::iny(uint16_t) {
+    m_y++;
+
+    // The negative status flag is set if the result is negative, i.e. has its most significant bit set.
+    setNegativeFlag(m_y);
+
+    // The zero flag is set if the result is zero, or cleared if it is non-zero.
+    setZeroFlag(m_y);
+}
+
+/* CLD (short for "CLear Decimal flag") is the mnemonic for a machine language instruction which clears the decimal
+ * flag. It complements the SED (set decimal flag) instruction.
+ */
+void C64::cld(uint16_t) {
+   m_sr &= 0xF7;
+}
+
+/* INX (short for "INcrease X") is the mnemonic for a machine language instruction which increases the numerical
+ * value held in the X index register by one, and "wraps over" when the numerical limits of a byte are exceeded.
+ */
+void C64::inx(uint16_t) {
+    m_x++;
+
+    // The negative status flag is set if the result is negative, i.e. has its most significant bit set.
+    setNegativeFlag(m_x);
+
+    // The zero flag is set if the result is zero, or cleared if it is non-zero.
+    setZeroFlag(m_x);
+}
+
+/* SED (short for "SEt Decimal flag") is the mnemonic for a machine language instruction which sets the decimal flag.
+ * It complements the CLD (clear decimal flag) instruction.
+ */
+void C64::sed(uint16_t) {
+    m_sr |= 0x08;
 }
--- c64.h
+++ c64.h
@@ -2,6 +2,14 @@
 
 #include <vector>
 #include <string>
+#include <array>
+#include <functional>
+
+struct Vector {
+    uint8_t lo;
+    uint8_t hi;
+    uint16_t toInt() const;
+};
 
 class C64 {
 
@@ -12,7 +20,45 @@ public:
     uint16_t m_pc;                  // program counter
     uint8_t m_a;                    // accumulator
     uint8_t m_x, m_y;               // x and y reg
+
+    /*
+     * m_sr represents the 65xx status register.
+     * Table 1 - Flags of the MOS Technology 65xx Processor Status Register
+     * Bit 	Flag 	          Abbreviation 	Purpose
+     * 0 	Carry Flag 	            C 	    Indicates when a bit of the result is to be carried to or borrowed from another byte. Also used for rotate and shift operations.
+       1 	Zero Flag 	            Z 	    A one indicates that the result of an operation is equal to zero.
+       2 	Interrupt Disable Flag 	I 	    If set IRQ will be prevented (masked), except non-maskable interrupts (NMI).
+       3 	Decimal Mode Flag 	    D 	    If set arithmetic operations are calculated in decimal mode (otherwise usually in binary mode).
+       4 	Break Command Flag 	    B 	    Indicates that interrupt request has been triggered by an BRK opcode (not an IRQ).
+       5 	Unused 	                - 	    Cannot be changed, usually 1.
+       6 	Overflow Flag 	        V 	    Indicates that a result of an signed arithmetic operation exceeds the signed value range (-128 to 127).
+       7 	Negative Flag 	        N 	    A value of 1 indicates that the result is negative (bit 7 is set, for a two's complement representation).
+    */
+    uint8_t m_sr;
+    uint8_t m_sp;                   // the stack pointer
     uint16_t vecAsInt(uint16_t);
-    uint8_t inx(uint8_t);
-    uint8_t iny(uint8_t);
+    Vector getVec(uint16_t);
+    uint8_t addr_inx(uint8_t);
+    uint8_t addr_iny(uint8_t);
+
+    std::array<std::function<void(int)>, 256> m_opcodes;
+    void setNegativeFlag(uint8_t);
+    void setZeroFlag(uint8_t);
+private:
+    void clc(uint16_t);
+    void sec(uint16_t);
+    void php(uint16_t);
+    void plp(uint16_t);
+    void pha(uint16_t);
+    void pla(uint16_t);
+    void cli(uint16_t);
+    void sei(uint16_t);
+    void dey(uint16_t);
+    void tya(uint16_t);
+    void tay(uint16_t);
+    void clv(uint16_t);
+    void iny(uint16_t);
+    void cld(uint16_t);
+    void inx(uint16_t);
+    void sed(uint16_t);
 };
